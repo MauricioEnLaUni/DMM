9d41b7a7ebab10e69f37f1c067a157ff5d99b450
VictorMendoza509 <109258504+VictorMendoza509@users.noreply.github.com>
2023-04-08 14:00:43 -0600

    Update tablas.sql
    add x_bus and Features

--- backEnd/database/tablas.sql
+++ backEnd/database/tablas.sql
@@ -13,6 +13,19 @@ CREATE TABLE `bus` (
 -- --------------------------------------------------------
 
 --
+-- Estructura de tabla para la tabla `features`
+--
+
+CREATE TABLE `features` (
+  `id_bus` int(11) NOT NULL,
+  `wifi` tinyint(1) NOT NULL,
+  `num_bathrooms` int(11) NOT NULL,
+  `individual_trunk` tinyint(1) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+-- --------------------------------------------------------
+
+--
 -- Estructura de tabla para la tabla `seats`
 --
 
@@ -55,7 +68,12 @@ CREATE TABLE `tickets` (
 
 CREATE TABLE `travel` (
   `id` int(11) NOT NULL,
-  `time` time NOT NULL
+  `name` varchar(50) NOT NULL,
+  `home` varchar(50) NOT NULL,
+  `home_time` time NOT NULL,
+  `destination` varchar(50) NOT NULL,
+  `destination_time` time NOT NULL,
+  `seats` int(11) NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
 
 -- --------------------------------------------------------
@@ -70,4 +88,18 @@ CREATE TABLE `users` (
   `last_name` text DEFAULT NULL,
   `password` varchar(85) NOT NULL,
   `telefono` text DEFAULT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+-- --------------------------------------------------------
+
+--
+-- Estructura de tabla para la tabla `x_bus`
+--
+
+CREATE TABLE `x_bus` (
+  `id_bus` int(11) NOT NULL,
+  `state` varchar(80) NOT NULL,
+  `seats` int(11) NOT NULL,
+  `luxury` int(11) NOT NULL,
+  `brand` varchar(80) NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;

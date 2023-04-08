4df6571287a4d77975ed2f2c71659742c42d1ba4
VictorMendoza509 <109258504+VictorMendoza509@users.noreply.github.com>
2023-04-08 14:01:38 -0600

    Update keys.sql
    Features and x_bus

--- backEnd/database/keys.sql
+++ backEnd/database/keys.sql
@@ -1,8 +1,18 @@
 --
+-- Índices para tablas volcadas
+--
+
+--
 -- Indices de la tabla `bus`
 --
 ALTER TABLE `bus`
   ADD PRIMARY KEY (`id`);
+
+--
+-- Indices de la tabla `features`
+--
+ALTER TABLE `features`
+  ADD KEY `features_ibfk_1` (`id_bus`);
 
 --
 -- Indices de la tabla `seats`
@@ -31,14 +41,22 @@ ALTER TABLE `tickets`
 -- Indices de la tabla `travel`
 --
 ALTER TABLE `travel`
-  ADD PRIMARY KEY (`id`);
+  ADD PRIMARY KEY (`id`),
+  ADD UNIQUE KEY `name` (`name`);
 
 --
 -- Indices de la tabla `users`
 --
 ALTER TABLE `users`
   ADD PRIMARY KEY (`id`),
-  ADD UNIQUE KEY `name` (`name`) USING HASH;
+  ADD UNIQUE KEY `name` (`name`) USING HASH,
+  ADD UNIQUE KEY `name_2` (`name`) USING HASH;
+
+--
+-- Indices de la tabla `x_bus`
+--
+ALTER TABLE `x_bus`
+  ADD KEY `id_bus` (`id_bus`);
 
 --
 -- AUTO_INCREMENT de las tablas volcadas
@@ -67,6 +85,12 @@ ALTER TABLE `users`
 --
 
 --
+-- Filtros para la tabla `features`
+--
+ALTER TABLE `features`
+  ADD CONSTRAINT `features_ibfk_1` FOREIGN KEY (`id_bus`) REFERENCES `bus` (`id`) ON DELETE CASCADE ON UPDATE CASCADE;
+
+--
 -- Filtros para la tabla `seats`
 --
 ALTER TABLE `seats`
@@ -84,4 +108,10 @@ ALTER TABLE `seats/travel`
 --
 ALTER TABLE `tickets`
   ADD CONSTRAINT `tickets_ibfk_1` FOREIGN KEY (`id_bus`) REFERENCES `bus` (`id`);
+
+--
+-- Filtros para la tabla `x_bus`
+--
+ALTER TABLE `x_bus`
+  ADD CONSTRAINT `x_bus_ibfk_1` FOREIGN KEY (`id_bus`) REFERENCES `bus` (`id`);
 COMMIT;

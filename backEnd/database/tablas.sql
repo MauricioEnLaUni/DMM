9fc2686ed52a9626c922927ccd33c7852a8b930d
VictorMendoza509 <109258504+VictorMendoza509@users.noreply.github.com>
2023-04-08 11:29:47 -0600

    Update tablas.sql
    add seats/travel

--- backEnd/database/tablas.sql
+++ backEnd/database/tablas.sql
@@ -1,27 +1,3 @@
--- phpMyAdmin SQL Dump
--- version 5.2.0
--- https://www.phpmyadmin.net/
---
--- Servidor: 127.0.0.1
--- Tiempo de generación: 07-04-2023 a las 21:28:44
--- Versión del servidor: 10.4.27-MariaDB
--- Versión de PHP: 8.2.0
-
-SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
-START TRANSACTION;
-SET time_zone = "+00:00";
-
-
-/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
-/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
-/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
-/*!40101 SET NAMES utf8mb4 */;
-
---
--- Base de datos: `bus`
---
-
-
 --
 -- Estructura de tabla para la tabla `bus`
 --
@@ -50,6 +26,18 @@ CREATE TABLE `seats` (
 -- --------------------------------------------------------
 
 --
+-- Estructura de tabla para la tabla `seats/travel`
+--
+
+CREATE TABLE `seats/travel` (
+  `id` int(11) NOT NULL,
+  `id_seats` int(11) NOT NULL,
+  `id_travel` int(11) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+-- --------------------------------------------------------
+
+--
 -- Estructura de tabla para la tabla `tickets`
 --
 
@@ -57,6 +45,17 @@ CREATE TABLE `tickets` (
   `id` int(11) NOT NULL,
   `id_bus` int(11) NOT NULL,
   `id_users` int(11) DEFAULT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+-- --------------------------------------------------------
+
+--
+-- Estructura de tabla para la tabla `travel`
+--
+
+CREATE TABLE `travel` (
+  `id` int(11) NOT NULL,
+  `time` time NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
 
 -- --------------------------------------------------------
@@ -72,12 +71,3 @@ CREATE TABLE `users` (
   `password` varchar(85) NOT NULL,
   `telefono` text DEFAULT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
-
---
--- Índices para tablas volcadas
---
-
-
-/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
-/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
-/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;

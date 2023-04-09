685f95d477d5171a1bfb298a454d42b894557b0a
MauricioEnLaUni <210804@utags.edu.mx>
2023-04-08 19:24:45 -0600

    React Proposal Started work on a react App

--- frontEnd/src/app/app.module.ts
+++ frontEnd/src/app/app.module.ts
@@ -1,10 +1,12 @@
-import { NgModule } from '@angular/core';
+import { NgModule, isDevMode } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 import { RouterModule, Routes } from '@angular/router';
 
 import { AppComponent } from './app.component';
 import { HomeComponent } from './home/home.component';
 import { LoginComponent } from './login/login.component';
+import { SeatComponent } from './seat/seat.component';
+import { ServiceWorkerModule } from '@angular/service-worker';
 
 const router: Routes = [
   { path: 'home', component: HomeComponent },
@@ -16,10 +18,17 @@ const router: Routes = [
     AppComponent,
     HomeComponent,
     LoginComponent,
+    SeatComponent,
   ],
   imports: [
     BrowserModule,
-    RouterModule.forRoot(router)
+    RouterModule.forRoot(router),
+    ServiceWorkerModule.register('ngsw-worker.js', {
+      enabled: !isDevMode(),
+      // Register the ServiceWorker as soon as the application is stable
+      // or after 30 seconds (whichever comes first).
+      registrationStrategy: 'registerWhenStable:30000'
+    })
   ],
   providers: [],
   bootstrap: [AppComponent]
--- frontEnd/src/main.ts
+++ frontEnd/src/main.ts
@@ -1,50 +1,6 @@
 import { platformBrowserDynamic } from '@angular/platform-browser-dynamic';
-import axios from './app/api/axios';
 
 import { AppModule } from './app/app.module';
-
-class Bus {
-  Id: string;
-  Name: string;
-
-  constructor(id: string, name: string)
-  {
-    this.Id = id;
-    this.Name = name;
-  }
-}
-
-const BUSSES: Array<Bus> = [
-  { Id: "1234", Name: "abcd" },
-  { Id: "1234", Name: "abcd" },
-  { Id: "1234", Name: "abcd" },
-  { Id: "1234", Name: "abcd" },
-  { Id: "1234", Name: "abcd" }
-]
-
-class Login_Dto {
-
-}
-
-const login = async () => {
-  const LOGIN_URL = '/auth';
-
-  try {
-    const DTO: Login_Dto = new Login_Dto(request);
-    const response = await axios.post(LOGIN_URL,
-      JSON.stringify(DTO),
-      {
-        headers: { 'Content-Type': 'application/json' },
-        withCredentials: true
-      }
-    );
-  } catch (e) {
-
-  }
-
-  console.log(response);
-  return response;
-}
 
 platformBrowserDynamic().bootstrapModule(AppModule)
   .catch(err => console.error(err));

020b8f822d05dde6400c7881288b230743f85e69
VictorMendoza509 <109258504+VictorMendoza509@users.noreply.github.com>
2023-04-08 23:33:16 -0600

    Update keys.sql

--- backEnd/database/keys.sql
+++ backEnd/database/keys.sql
@@ -1,4 +1,11 @@
 --
+-- Estructura para la vista `trips`
+--
+DROP TABLE IF EXISTS `trips`;
+
+CREATE ALGORITHM=UNDEFINED DEFINER=`root`@`localhost` SQL SECURITY DEFINER VIEW `trips`  AS SELECT `trip`.`id` AS `id`, `trip`.`name` AS `name`, `trip`.`home` AS `home`, `trip`.`departure` AS `departure`, `trip`.`destination` AS `destination`, `trip`.`arrival` AS `arrival`, `trip`.`seats` AS `seats` FROM `trip` WHERE `trip`.`departure` > current_timestamp() AND `trip`.`departure` <= current_timestamp() + interval 10 minute LIMIT 0, 2525  ;
+
+--
 -- Índices para tablas volcadas
 --
 
@@ -22,27 +29,17 @@ ALTER TABLE `seats`
   ADD KEY `id_bus` (`id_bus`);
 
 --
--- Indices de la tabla `seats/travel`
+-- Indices de la tabla `seats_trip`
 --
-ALTER TABLE `seats/travel`
-  ADD PRIMARY KEY (`id`),
+ALTER TABLE `seats_trip`
   ADD KEY `id_seats` (`id_seats`),
   ADD KEY `id_travel` (`id_travel`);
 
 --
--- Indices de la tabla `tickets`
+-- Indices de la tabla `trip`
 --
-ALTER TABLE `tickets`
-  ADD PRIMARY KEY (`id`),
-  ADD KEY `id_bus` (`id_bus`),
-  ADD KEY `id_users` (`id_users`);
-
---
--- Indices de la tabla `travel`
---
-ALTER TABLE `travel`
-  ADD PRIMARY KEY (`id`),
-  ADD UNIQUE KEY `name` (`name`);
+ALTER TABLE `trip`
+  ADD PRIMARY KEY (`id`);
 
 --
 -- Indices de la tabla `users`
@@ -51,6 +48,13 @@ ALTER TABLE `users`
   ADD PRIMARY KEY (`id`),
   ADD UNIQUE KEY `name` (`name`) USING HASH,
   ADD UNIQUE KEY `name_2` (`name`) USING HASH;
+
+--
+-- Indices de la tabla `users_bus`
+--
+ALTER TABLE `users_bus`
+  ADD KEY `id_bus` (`id_bus`),
+  ADD KEY `id_users` (`id_users`);
 
 --
 -- Indices de la tabla `x_bus`
@@ -69,10 +73,10 @@ ALTER TABLE `seats`
   MODIFY `id` int(11) NOT NULL AUTO_INCREMENT;
 
 --
--- AUTO_INCREMENT de la tabla `travel`
+-- AUTO_INCREMENT de la tabla `trip`
 --
-ALTER TABLE `travel`
-  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT;
+ALTER TABLE `trip`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=169;
 
 --
 -- AUTO_INCREMENT de la tabla `users`
@@ -97,17 +101,17 @@ ALTER TABLE `seats`
   ADD CONSTRAINT `seats_ibfk_1` FOREIGN KEY (`id_bus`) REFERENCES `bus` (`id`) ON DELETE CASCADE ON UPDATE CASCADE;
 
 --
--- Filtros para la tabla `seats/travel`
+-- Filtros para la tabla `seats_trip`
 --
-ALTER TABLE `seats/travel`
-  ADD CONSTRAINT `seats/travel_ibfk_1` FOREIGN KEY (`id_seats`) REFERENCES `seats` (`id`),
-  ADD CONSTRAINT `seats/travel_ibfk_2` FOREIGN KEY (`id_travel`) REFERENCES `travel` (`id`);
+ALTER TABLE `seats_trip`
+  ADD CONSTRAINT `seats_trip_ibfk_1` FOREIGN KEY (`id_seats`) REFERENCES `seats` (`id`),
+  ADD CONSTRAINT `seats_trip_ibfk_2` FOREIGN KEY (`id_travel`) REFERENCES `trip` (`id`);
 
 --
--- Filtros para la tabla `tickets`
+-- Filtros para la tabla `users_bus`
 --
-ALTER TABLE `tickets`
-  ADD CONSTRAINT `tickets_ibfk_1` FOREIGN KEY (`id_bus`) REFERENCES `bus` (`id`);
+ALTER TABLE `users_bus`
+  ADD CONSTRAINT `users_bus_ibfk_1` FOREIGN KEY (`id_bus`) REFERENCES `bus` (`id`);
 
 --
 -- Filtros para la tabla `x_bus`

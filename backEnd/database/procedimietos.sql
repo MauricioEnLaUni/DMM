f1a889e5bc22aa3e462ff5aa1f0c831c43e48c06
VictorMendoza509 <109258504+VictorMendoza509@users.noreply.github.com>
2023-04-08 23:29:46 -0600

    Update procedimietos.sql

--- backEnd/database/procedimietos.sql
+++ backEnd/database/procedimietos.sql
@@ -12,31 +12,30 @@ CREATE DEFINER=`root`@`localhost` PROCEDURE `buses` ()   begin
 end$$
 
 CREATE DEFINER=`root`@`localhost` PROCEDURE `cancel` (IN `i` INT(11))   begin 
-	delete from tickets where id=i;
+	delete from users_bus where id_user=i;
 end$$
 
 CREATE DEFINER=`root`@`localhost` PROCEDURE `insert_user` (`na` TEXT, `pass` VARCHAR(15))   begin
 	insert into users (`name`,`password`) values (`na`,`pass`);
 end$$
 
-CREATE DEFINER=`root`@`localhost` PROCEDURE `modify` (IN `i` INT(11), `bus` INT(11), `tickets` INT(11))   begin 
-	update tickets set id_bus=bus, `id_users`=tickets where id=i;
+CREATE DEFINER=`root`@`localhost` PROCEDURE `modify` (IN `i` INT(11), IN `bus` INT(11), IN `user` INT(11))   begin 
+	update users_bus set id_bus=bus where `id_users`=user;
 end$$
 
-CREATE DEFINER=`root`@`localhost` PROCEDURE `sale` (IN `i` INT(11), `bus` INT(11), `tickets` INT(11), `idu` INT)   begin 
-	insert into tickets values (`i`,`bus`,`tickets`);
-    update users set `n_ticket`= tickets where id=idu;
+CREATE DEFINER=`root`@`localhost` PROCEDURE `sale` (`i` INT, `bus` INT)   begin 
+   insert into users_bus values (id_user= i, id_bus= bus);
 end$$
 
 CREATE DEFINER=`root`@`localhost` PROCEDURE `seatsbyuser` (IN `idu` INT)   BEGIN
     SELECT * FROM seats where id=(select id from users where id=idu);
 END$$
 
-CREATE DEFINER=`root`@`localhost` PROCEDURE `trips` (IN `idu` INT)   begin 
-	select * from tickets where `id_users`=(select id from users where id=idu);
-end$$
+CREATE DEFINER=`root`@`localhost` PROCEDURE `trips` ()   BEGIN
+    SELECT * FROM TRIP WHERE (SELECT DATE_ADD(NOW(), INTERVAL 10 MINUTE_SECOND))limit 25;
+END$$
 
-CREATE DEFINER=`root`@`localhost` PROCEDURE `users` (IN `idu` INT)   BEGIN
+CREATE DEFINER=`root`@`localhost` PROCEDURE `usersbyid` (IN `idu` INT)   BEGIN
     SELECT * FROM users where id=idu;
 END$$
 
@@ -47,3 +46,4 @@ END$$
 DELIMITER ;
 
 -- --------------------------------------------------------
+

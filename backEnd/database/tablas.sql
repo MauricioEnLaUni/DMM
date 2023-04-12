fc4c82ac1e436b76a72510f5e6ad5dfa25325b0d
VictorMendoza509 <109258504+VictorMendoza509@users.noreply.github.com>
2023-04-11 22:08:55 -0600

    Update tablas.sql

--- backEnd/database/tablas.sql
+++ backEnd/database/tablas.sql
@@ -1,31 +1,4 @@
 --
--- Estructura de tabla para la tabla `bus`
---
-
-CREATE TABLE `bus` (
-  `id` int(11) NOT NULL,
-  `home` varchar(25) NOT NULL,
-  `departure` datetime NOT NULL,
-  `destination` varchar(25) NOT NULL,
-  `arrival` datetime NOT NULL
-) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
-
--- --------------------------------------------------------
-
---
--- Estructura de tabla para la tabla `features`
---
-
-CREATE TABLE `features` (
-  `id_bus` int(11) NOT NULL,
-  `wifi` tinyint(1) NOT NULL,
-  `num_bathrooms` int(11) NOT NULL,
-  `individual_trunk` tinyint(1) NOT NULL
-) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
-
--- --------------------------------------------------------
-
---
 -- Estructura de tabla para la tabla `seats`
 --
 
@@ -36,26 +9,13 @@ CREATE TABLE `seats` (
   `assigned` tinyint(1) NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
 
--- --------------------------------------------------------
-
---
--- Estructura de tabla para la tabla `seats_trip`
---
-
-CREATE TABLE `seats_trip` (
-  `id_seats` int(11) NOT NULL,
-  `id_trip` int(11) NOT NULL
-) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
-
--- --------------------------------------------------------
-
 --
 -- Estructura de tabla para la tabla `tickets`
 --
 
 CREATE TABLE `tickets` (
   `id` int(11) NOT NULL,
-  `id_bus` int(11) DEFAULT NULL,
+  `id_trip` int(11) DEFAULT NULL,
   `id_usr` int(11) DEFAULT NULL,
   `seats` int(11) DEFAULT NULL,
   `price` float(11,2) DEFAULT NULL
@@ -79,8 +39,6 @@ CREATE TABLE `trip` (
   `seats` int(11) NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
 
--- --------------------------------------------------------
-
 --
 -- Estructura de tabla para la tabla `users`
 --
@@ -91,29 +49,4 @@ CREATE TABLE `users` (
   `last_name` text DEFAULT NULL,
   `password` varchar(85) NOT NULL,
   `telefono` text DEFAULT NULL
-) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
-
--- --------------------------------------------------------
-
---
--- Estructura de tabla para la tabla `users_bus`
---
-
-CREATE TABLE `users_bus` (
-  `id_bus` int(11) NOT NULL,
-  `id_users` int(11) DEFAULT NULL
-) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
-
--- --------------------------------------------------------
-
---
--- Estructura de tabla para la tabla `x_bus`
---
-
-CREATE TABLE `x_bus` (
-  `id_bus` int(11) NOT NULL,
-  `state` varchar(80) NOT NULL,
-  `seats` int(11) NOT NULL,
-  `luxury` int(11) NOT NULL,
-  `brand` varchar(80) NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
